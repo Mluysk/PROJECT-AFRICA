b04254e5fb9e1c1c120918b1cfdf3d19b304c8a1
Mluysk <148017397+Mluysk@users.noreply.github.com>
2026-01-01 14:44:26 -0300

    Update footer brand logo and remove note

--- footer.php
+++ footer.php
@@ -2,16 +2,15 @@
   <div class="container">
     <div class="foot">
       <div class="col">
-        <b>Projecto África</b>
-        <div style="margin-top:10px; line-height:1.6">
-          Transparência, ação local e respeito.<br/>
-          Contato: <span style="color:rgba(255,255,255,.82)">contato@projectoafrica.org</span>
-        </div>
-      </div>
-      <div class="col">
-        <b>Nota</b>
-        <div style="margin-top:10px; line-height:1.6">
-          Doações e comentários ficam salvos no servidor em arquivos JSON (sem MySQL).
+        <div class="footer-brand">
+          <img class="footer-logo" src="/logo.png" alt="Projecto África" />
+          <div>
+            <b>Projecto África</b>
+            <div style="margin-top:10px; line-height:1.6">
+              Transparência, ação local e respeito.<br/>
+              Contato: <span style="color:rgba(255,255,255,.82)">contato@projectoafrica.org</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
